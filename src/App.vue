232b1623fa721002d6e1865e6386739660fa1773
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-07-20 10:43:12 +0700

    habit card

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Icon } from '@iconify/vue';
 import AppNavbar from './components/partials/AppNavbar.vue';
 
 const activities = [
@@ -6,59 +7,103 @@ const activities = [
     name: 'Ngoding Project',
     done: 3,
     target: 0,
-    targetPeriod: null,
+    reset: 'daily',
+    icon: 'twemoji:laptop-computer',
   },
   {
     name: 'Ngaji',
     done: 2,
     target: 5,
-    targetPeriod: 'daily',
+    reset: 'daily',
+    icon: 'twemoji:open-book',
   },
   {
     name: 'Shalat Wajib di Masjid',
     done: 3,
     target: 5,
-    targetPeriod: 'daily',
+    reset: 'daily',
+    icon: 'twemoji:mosque',
   },
   {
     name: 'Nulis Blog',
     done: 1,
     target: 1,
-    targetPeriod: 'daily',
+    reset: 'daily',
+    icon: 'twemoji:writing-hand',
   },
   {
     name: 'Baca Dorar',
     done: 1,
     target: 0,
-    targetPeriod: null,
+    reset: 'daily',
+    icon: 'twemoji:thread',
   },
   {
     name: 'Baca Blog',
     done: 1,
     target: 0,
-    targetPeriod: null,
+    reset: 'daily',
+    icon: 'twemoji:bookmark-tabs',
   },
 ];
 </script>
 
 <template>
-  <div class="text-slate-900 min-h-screen dark:bg-slate-900 dark:text-white">
+  <div
+    class="text-gray-900 min-h-screen bg-gray-100 dark:bg-gray-900 dark:text-white"
+  >
     <AppNavbar />
-    <ul>
-      <li v-for="activity in activities" :key="activity.name">
-        <p>{{ activity.name }}</p>
-        <button>Action</button>
-        <template v-if="activity.target">
-          <progress :max="activity.target" :value="activity.done"></progress>
-          <p>Target: {{ activity.target }}/{{ activity.targetPeriod }}</p>
-        </template>
-        <p>
-          <button>-</button>
-          {{ activity.done }}x
-          <button>+</button>
-        </p>
-      </li>
-    </ul>
-    <button>Tambah Habbit</button>
+    <div class="container px-4 mx-auto py-6">
+      <div class="space-y-4">
+        <div
+          v-for="activity in activities"
+          :key="activity.name"
+          class="bg-white border border-gray-200 rounded-lg p-4 space-y-4"
+        >
+          <div class="flex items-center justify-between">
+            <p class="font-bold flex items-center gap-2 text-lg">
+              <Icon :icon="activity.icon" />
+              {{ activity.name }}
+            </p>
+            <button class="text-gray-500">
+              <Icon icon="tabler:dots" />
+            </button>
+          </div>
+          <div v-if="activity.target" class="space-y-1">
+            <div class="w-full h-2 bg-gray-100 rounded">
+              <div
+                class="bg-blue-500 h-full rounded"
+                :style="{
+                  width: `${(activity.done / activity.target) * 100}%`,
+                }"
+              ></div>
+            </div>
+            <div
+              class="flex items-center justify-between text-sm text-gray-500"
+            >
+              <span>{{ activity.done }}/{{ activity.target }}</span>
+              <span>{{ (activity.done / activity.target) * 100 }} %</span>
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <p class="text-sm text-gray-500">Hari ini</p>
+            <div class="flex items-center justify-end gap-2">
+              <button
+                class="w-8 h-8 flex items-center justify-center rounded border border-gray-200"
+              >
+                <Icon icon="tabler:minus" />
+              </button>
+              <p class="font-bold text-xl">{{ activity.done }}x</p>
+              <button
+                class="w-8 h-8 flex items-center justify-center rounded border border-gray-200"
+              >
+                <Icon icon="tabler:plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button>Tambah Habbit</button>
+    </div>
   </div>
 </template>
--- src/components/partials/AppNavbar.vue
+++ src/components/partials/AppNavbar.vue
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/vue';
 
 <template>
   <nav
-    class="border-b border-slate-300 h-14 flex items-center dark:border-slate-800"
+    class="bg-white border-b border-gray-200 h-14 flex items-center dark:border-gray-800"
   >
     <div class="container px-4 mx-auto flex items-center justify-between">
       <a href="" class="flex items-center gap-2 font-bold">
