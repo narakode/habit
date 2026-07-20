17362bf8dfee6376189ec94102447f4d8a374740
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-07-20 16:15:04 +0700

    instal floating vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import 'floating-vue/dist/style.css';
+import App from './App.vue';
+import FloatingVue from 'floating-vue';
 
-createApp(App).mount('#app')
+createApp(App).use(FloatingVue).mount('#app');
