eca90a2af10bd5a044fc3ce4929a93502d785764
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-07-26 13:35:56 +0700

    meta

--- src/core/router.js
+++ src/core/router.js
@@ -6,17 +6,33 @@ export const router = createRouter({
     {
       path: '/',
       name: 'home',
+      meta: {
+        title: 'Home',
+      },
       component: () => import('../features/habit/pages/HabitIndexPage.vue'),
     },
     {
       path: '/stats',
       name: 'stats',
+      meta: {
+        title: 'Stats',
+      },
       component: () => import('../features/stats/pages/StatsIndexPage.vue'),
     },
     {
       path: '/history',
       name: 'history',
+      meta: {
+        title: 'History',
+      },
       component: () => import('../features/history/pages/HistoryIndexPage.vue'),
     },
   ],
+});
+
+router.beforeEach((to) => {
+  const { title } = to.meta;
+  const appTitle = import.meta.env.VITE_APP_TITLE;
+
+  document.title = title ? `${title} - ${appTitle}` : appTitle;
 });
